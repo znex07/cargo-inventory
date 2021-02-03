4594ded62a73d8b230164d72fbaf26719b5c5528
znex07 <znex07@gmail.com>
2021-02-03 16:09:26 +0800

    initial

---
+++ database/migrations/2021_02_03_075920_create_cargos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCargosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cargos', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('cargo_code');
+            $table->string('cargo_description');
+            $table->string('cargo_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cargos');
+    }
+}
